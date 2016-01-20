ca3baf64c35c1f4948e334a8a229e8df35b6f1a2
andrew cho and ryan brown <browneryan87@gmail.com>
2016-01-20 14:28:15 -0800

    Add checkConsonant function to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
-// var vowel = ["a","e","i","o","u"]; {
-//   // did not include y on purpose
-// }
-
+var vowel = ["a","e","i","o","u"];
 
 function checkVowel (string) {
-  var vowel = ["a","e","i","o","u"];
   for (var i = 0; i < vowel.length; i++) {
-    debugger;
     if (string[0] === vowel[i]) {
-      return true;
+      return string + "ay";
+    }
+  };
+};
+
+function checkConsonant (string) {
+  for (var j = 0; j < string.length; j++) {
+    for (var i = 0; i < vowel.length; i++) {
+      if (string.charAt([j]) !== vowel[i]) {
+        //once you reach the first vowel in the string, the else statement begins
+      } else {
+        return string.slice([j]) + string.slice(0,[(j)]) + "ay";
+      }
+      }
     }
   }
+ // !== vowel
 
-};
+ // else {
+ // for (var i = 0; i = vowel[i]; i++) {
+ //   vowel[i].contains('s');
+ //   return i;
+ //   break;
+ //  }
+//
+// for (var i = 0; i < vowel.length; i++) {
+//   for (var j = 0; j < string.length; j++ ) {
+//     if (string.charAt)
+//   }
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,18 @@
 describe('checkVowel', function(){
   it("is true that the string starts with a vowel", function(){
-    expect(checkVowel("apple")).to.equal(true);
+    expect(checkVowel("apple")).to.equal("appleay");
   });
 });
+
+describe('checkConsonant', function(){
+  it("is true that the beginning consonants will add to the end of word", function(){
+    expect(checkConsonant("strawberry")).to.equal("awberrystray");
+  });
+});
+// it("is false that the string starts with a vowel", function(){
+//   expect(checkVowel("brownies")).to.equal(false);
+// });
+
+// it("is true that the string starts with a consonant", function(){
+//   expect(checkVowel("straight")).to.equal(3);
+// });
